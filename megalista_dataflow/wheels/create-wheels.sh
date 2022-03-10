6872b6db1d7f38515df64bf1216c418334e6383c
Antônio Moreira <antoniomoreira@google.com>
2022-03-10 16:19:05 -0300

    Fix pip wheel requirements file
    Change-Id: Ic1400164a9e93625b33bdd6d136c6dcfb05fe02c

--- megalista_dataflow/wheels/create-wheels.sh
+++ megalista_dataflow/wheels/create-wheels.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
-pip wheel ./setup.py -w ./wheels/packages
+pip wheel -r ./wheels/requirements.txt -w ./wheels/packages
 #pip wheel --no-deps -r ./megalista_dataflow/requirements-no-deps.txt -w ./wheels/packages
